02f7e917cf68501897b3dee5a830e5ce29af329a
sherwinski <sherwinhb@gmail.com>
2024-06-19 16:04:37 -0400

    wrap license plate search input in a form for better usability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,46 +15,47 @@ export default function Home() {
   const [isSearched, setIsSearched] = useState<boolean>(false)
 
   return (
-    <div className="flex flex-col pt-12 min-h-screen justify-between gap-12">
-      <div className="flex flex-col gap-12 items-center justify-between">
-        <div>
-          <p className="text-4xl text-slate-300">
-            Stolen Vehicle Search Database
-          </p>
-        </div>
+    <form
+      onSubmit={async (e) => {
+        e.preventDefault()
+        const foundVehicles = await searchForVehicles(licensePlateText)
+        setVehicles(foundVehicles)
+        setIsSearched(true)
+      }}
+    >
+      <div className="flex flex-col pt-12 min-h-screen justify-between gap-12">
+        <div className="flex flex-col gap-12 items-center justify-between">
+          <div>
+            <p className="text-4xl text-slate-300">
+              Stolen Vehicle Search Database
+            </p>
+          </div>
 
-        <div className="flex flex-col gap-4">
-          <Label htmlFor="licensePlate" className="text-lg text-slate-300">
-            Enter a License Plate
-          </Label>
-          <div className="flex gap-4">
-            <Input
-              id="licensePlate"
-              type="text"
-              placeholder="Plate Number"
-              value={licensePlateText}
-              onChange={(e) => setLicensePlateText(e.target.value)}
-            />
-            <Button
-              onClick={async () => {
-                const foundVehicles = await searchForVehicles(licensePlateText)
-                setVehicles(foundVehicles)
-                setIsSearched(true)
-              }}
-            >
-              Search
-            </Button>
+          <div className="flex flex-col gap-4">
+            <Label htmlFor="licensePlate" className="text-lg text-slate-300">
+              Enter a License Plate
+            </Label>
+            <div className="flex gap-4">
+              <Input
+                id="licensePlate"
+                type="text"
+                placeholder="Plate Number"
+                value={licensePlateText}
+                onChange={(e) => setLicensePlateText(e.target.value)}
+              />
+              <Button type="submit">Search</Button>
+            </div>
           </div>
         </div>
+
+        {isSearched && (
+          <div className="flex flex-col gap-8 w-full items-center">
+            <PhotoGrid vehicles={vehicles} />
+          </div>
+        )}
+
+        <Footer />
       </div>
-
-      {isSearched && (
-        <div className="flex flex-col gap-8 w-full items-center">
-          <PhotoGrid vehicles={vehicles} />
-        </div>
-      )}
-
-      <Footer />
-    </div>
+    </form>
   )
 }
