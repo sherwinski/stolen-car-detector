61b3f9cd95d4c0fe3adc55efb202ab4d3a8a6754
sherwinski <sherwinhb@gmail.com>
2024-06-19 15:58:16 -0400

    fix eslint issue, no unescaped quotes

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,9 +3,9 @@ export function Footer() {
     <div className="flex flex-col gap-4 mb-32 text-center lg:mb-0 lg:w-full lg:grid-cols-4 lg:text-left sticky bottom-0 py-12 px-24 bg-background opacity-80">
       Made with Roboflow 🤖
       <p className="text-slate-500">
-        <b>Hint:</b> If you want to test something quickly, try searching for{' '}
-        <code className="italic text-orange-500">"ALI"</code>. Alternatively,
-        you could try an empty search.
+        <b>Hint:</b> If you want to test something quickly, try searching for
+        <code className="italic text-orange-500"> &quot;ALI&quot;</code>.
+        Alternatively, you could try an empty search.
       </p>
     </div>
   )
